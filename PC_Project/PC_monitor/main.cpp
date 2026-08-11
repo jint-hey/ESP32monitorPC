3116c66805c86ec28a0af7b30336abfdaca00c24
jint <165740680+jint-hey@users.noreply.github.com>
2026-08-11 11:00:56 +0800

    update main.cpp

--- PC_Project/PC_monitor/main.cpp
+++ PC_Project/PC_monitor/main.cpp
@@ -19,6 +19,57 @@ namespace
     constexpr bool ENABLE_DEBUG_LOGGING = false;
 #endif
 
+
+    constexpr const wchar_t* SINGLE_INSTANCE_MUTEX_NAME =
+        L"Local\\PC_monitor_{28CEC82D-439F-4397-84B3-AE0D5DB83316}";
+
+    constexpr const wchar_t* APPLICATION_NAME =
+        L"PC Hardware Monitor";
+
+    class SingleInstanceGuard
+    {
+    public:
+        SingleInstanceGuard()
+        {
+            handle_ = CreateMutexW(
+                nullptr,
+                FALSE,
+                SINGLE_INSTANCE_MUTEX_NAME
+            );
+
+            if (handle_ != nullptr)
+            {
+                alreadyExists_ =
+                    GetLastError() == ERROR_ALREADY_EXISTS;
+            }
+        }
+
+        ~SingleInstanceGuard()
+        {
+            if (handle_ != nullptr)
+            {
+                CloseHandle(handle_);
+            }
+        }
+
+        SingleInstanceGuard(const SingleInstanceGuard&) = delete;
+        SingleInstanceGuard& operator=(const SingleInstanceGuard&) = delete;
+
+        bool IsValid() const
+        {
+            return handle_ != nullptr;
+        }
+
+        bool AlreadyExists() const
+        {
+            return alreadyExists_;
+        }
+
+    private:
+        HANDLE handle_ = nullptr;
+        bool alreadyExists_ = false;
+    };
+
     class DebugConsole
     {
     public:
@@ -128,6 +179,35 @@ int WINAPI wWinMain(
 {
     UNREFERENCED_PARAMETER(previousInstance);
     UNREFERENCED_PARAMETER(commandLine);
+
+
+    SingleInstanceGuard singleInstance;
+
+    if (!singleInstance.IsValid())
+    {
+        MessageBoxW(
+            nullptr,
+            L"Unable to create the single-instance guard.",
+            APPLICATION_NAME,
+            MB_OK | MB_ICONERROR | MB_SETFOREGROUND
+        );
+
+        return 1;
+    }
+
+    if (singleInstance.AlreadyExists())
+    {
+        MessageBoxW(
+            nullptr,
+            L"PC Hardware Monitor \u5df2\u5728\u540e\u53f0\u8fd0\u884c\u3002",
+            APPLICATION_NAME,
+            MB_OK |
+                MB_ICONINFORMATION |
+                MB_SETFOREGROUND
+        );
+
+        return 0;
+    }
 
     DebugConsole debugConsole(
         ENABLE_DEBUG_LOGGING
